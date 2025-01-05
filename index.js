d442a7184add3551168c55d9bc3fa6e0f051e994
JayaniPerera27 <pererajayani27@gmail.com>
2025-01-05 10:01:15 +0530

    connect orderRouter.js file into index.js file

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ import userRouter from './routes/userRouter.js';
 import jwt from "jsonwebtoken";
 import dotenv from "dotenv";
 import productRouter from './routes/productRouter.js';
+import orderRouter from './routes/orderRouter.js';
 
 dotenv.config();
 
@@ -46,6 +47,7 @@ app.use((req, res, next) => {
 
 app.use("/api/users",userRouter)
 app.use("/api/products",productRouter)
+app.use("/api/orders",orderRouter)
 
 
 app.listen(
