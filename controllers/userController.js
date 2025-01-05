044559177da59caf0184db5d9829af5b2731d759
JayaniPerera27 <pererajayani27@gmail.com>
2025-01-05 07:54:17 +0530

    create validation function

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,89 +2,114 @@ import User from "../models/user.js";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 import dotenv from "dotenv";
-
 dotenv.config()
-
 export function createUser(req,res){
-    
-    const newUserData = req.body
 
-    if(newUserData.type == "admin"){
+  const newUserData = req.body
 
-        if(req.user==null){
-            res.json({
-                message: "Please login as a administartor to create admin accounts"
-            })
-            return
-        }
-        if(req.user.type !="admin" ){
-            res.json({
-                message: "Please login as administrator to create admin accounts"
-            })
-            return
-        }
+  if(newUserData.type == "admin"){
+
+    if(req.user==null){
+      res.json({
+        message: "Please login as administrator to create admin accounts"
+      })
+      return
     }
 
-    newUserData.password = bcrypt.hashSync(newUserData.password,10)
+    if(req.user.type != "admin"){
+      res.json({
+        message: "Please login as administrator to create admin accounts"
+      })
+      return
+    }
 
-    const user = new User(newUserData)
+  }
 
-    user.save().then(()=>{
-        res.json({
-            message : "User created"
-        })
-    }).catch(()=>{
-        res.json({
-            message : "User not created"
-        })
+  newUserData.password = bcrypt.hashSync(newUserData.password, 10)  
+
+  const user = new User(newUserData)
+
+  user.save().then(()=>{
+    res.json({
+      message: "User created"
     })
+  }).catch((error)=>{
+    res.json({      
+      message: "User not created"
+    })
+  })
+  
 }
 
 export function loginUser(req,res){
-    User.find({email : req.body.email}).then(
-        (users)=>{
-            if(users.length == 0){
-                res.json({
-                    message : "User not found"
-                })
-            }else{
 
-                const user = users[0]
+  User.find({email : req.body.email}).then(
+    (users)=>{
+      if(users.length == 0){
 
-                const isPasswordCorrect = bcrypt.compareSync
-                (req.body.password,user.password)
+        res.json({
+          message: "User not found"
+        })
 
-                if(isPasswordCorrect){
-                   
-                    const token = jwt.sign({
-                        email : user.email,
-                        firstName : user.firstName,
-                        lastName : user.lastName,
-                        isBlocked : user.isBlocked,
-                        type : user.type,
-                        profilepicture : user.profilepicture
-                    }, process.env.SECRET)
+      }else{
 
-                    res.json({
-                        message: "User logged in",
-                        token : token
-                    })
-                }else{
-                    res.json({
-                        message : "User not logged in (wrong password)"
-                    })
-                }
+        const user = users[0]
+
+        const isPasswordCorrect = bcrypt.compareSync(req.body.password,user.password)
+
+        if(isPasswordCorrect){
+
+          const token = jwt.sign({
+            email : user.email,
+            firstName : user.firstName,
+            lastName : user.lastName,
+            isBlocked : user.isBlocked,
+            type : user.type,
+            profilePicture : user.profilePicture
+          } , process.env.SECRET)
+          
+          res.json({
+            message: "User logged in",
+            token: token,
+            user : {
+              firstName : user.firstName,
+              lastName : user.lastName,
+              type : user.type,
+              profilePicture : user.profilePicture,
+              email : user.email
             }
-           
+          })
+          
+        }else{
+          res.json({
+            message: "User not logged in (wrong password)"
+          })
         }
-    )
+      }
+    }
+  )
 }
 
-export function deleteUser(req,res){
-    User.deleteOne({email : req.body.email}).then(()=>{
-        res.json({
-            message : "Student deleted"
-        })
-    })
+export function isAdmin(req){
+  if(req.user==null){
+    return false
+  }
 
+  if(req.user.type != "admin"){
+    return false
+  }
+
+  return true
+}
+
+export function isCustomer(req){
+  if(req.user==null){
+    return false
+  }
+
+  if(req.user.type != "customer"){
+    return false
+  }
+
+  return true
 }
