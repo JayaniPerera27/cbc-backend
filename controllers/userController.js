bd4f431743da2e9e34c9e0e4bcb73c895b59b6ca
JayaniPerera27 <pererajayani27@gmail.com>
2025-01-05 07:37:41 +0530

    enhanced admin login

--- controllers/userController.js
+++ controllers/userController.js
@@ -9,6 +9,22 @@ export function createUser(req,res){
     
     const newUserData = req.body
 
+    if(newUserData.type == "admin"){
+
+        if(req.user==null){
+            res.json({
+                message: "Please login as a administartor to create admin accounts"
+            })
+            return
+        }
+        if(req.user.type !="admin" ){
+            res.json({
+                message: "Please login as administrator to create admin accounts"
+            })
+            return
+        }
+    }
+
     newUserData.password = bcrypt.hashSync(newUserData.password,10)
 
     const user = new User(newUserData)
--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
-import productRouter from './routes/productRouter.js';
+
 import userRouter from './routes/userRouter.js';
 import jwt from "jsonwebtoken";
 import dotenv from "dotenv";
@@ -41,7 +41,7 @@ app.use((req, res, next) => {
 });
 
 
-app.use("/api/products",productRouter)
+
 
 app.use("/api/users",userRouter)
 
