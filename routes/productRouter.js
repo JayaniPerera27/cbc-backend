ad633ae86152ca73cae7a162e585e18d9c0ab616
JayaniPerera27 <pererajayani27@gmail.com>
2025-01-05 08:06:04 +0530

    create productRouter

---
+++ routes/productRouter.js
@@ -0,0 +1,11 @@
+import express from 'express';
+import { createProduct, deleteProduct, getProducts } from '../controllers/productController.js';
+
+const productRouter = express.Router();
+
+productRouter.post("/",createProduct)
+productRouter.get("/",getProducts)
+//productRouter.delete("/:productId",deleteProduct)
+
+
+export default productRouter; 
